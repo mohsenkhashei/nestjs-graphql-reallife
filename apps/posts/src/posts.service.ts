00cf324552ea1d3d1da6073d22aef4fc66fc6158
MohsenKhashei <m.khasheii@gmail.com>
2023-06-04 19:08:33 +0300

    add  post

--- apps/posts/src/posts.service.ts
+++ apps/posts/src/posts.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePostInput } from './dto/create-post.input';
+import { Post } from './entities/post.entity';
 
 @Injectable()
 export class PostsService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly posts: Post[] = [];
+  create(createPostInput: CreatePostInput) {
+    this.posts.push(createPostInput);
+    return createPostInput;
+  }
+
+  findAll() {
+    return this.posts;
+  }
+
+  findOne(id: string) {
+    return this.posts.find((post) => post.id === id);
   }
 }
